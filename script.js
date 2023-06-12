a8d1a30569ff7ce508f248966c92bfccf9847754
Rafael Encinas <rafaeladalbertoencinas94@gmail.com>
2023-06-12 10:33:30 -0700

    Add age verification modal, and two sections
    Added an age verification modal, that saves the user answer in session
    storage to prevent the modal from displaying if the users had already
    verified their age.
    
    Added "Where to buy" and "cards" sections, but is still missing
    responsiveness, will be updated on next commit.
    
    Navbar and footer background color has been changed to #222222

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 console.log("Test")
+
+let checkAge = sessionStorage.getItem("verifiedAge");
+const mayorEdadBtn = document.querySelector("#mayorEdad");
+const menorEdadBtn = document.querySelector("#menorEdad");
+const verifyAgeLocation = document.querySelector("#verifyAge");
+
+mayorEdadBtn.addEventListener("click",()=>{
+    console.log("Mayor de edad")
+    verifyAgeLocation.remove();
+    sessionStorage.setItem("verifiedAge", "older");
+    checkAge = sessionStorage.getItem("verifiedAge");
+    console.log(checkAge);
+
+})
+
+menorEdadBtn.addEventListener("click",()=>{
+    console.log("Menor de edad")
+})
+
+if(checkAge=="older"){
+    console.log("My dude is 21+");
+    verifyAgeLocation.remove();
+}
+else{
+    console.log("younger than 21");
+}
