56261226ef788fb5bc6daf835fd5f79e8ccf0529
Rafael Encinas <rafaeladalbertoencinas94@gmail.com>
2023-06-12 16:21:38 -0700

    Add responsive product cards to Products page
    Created a product cards template, using "clip-path: inset" for rounded
    borders.

--- assets/scripts/navbar.js
+++ assets/scripts/navbar.js
@@ -3,7 +3,7 @@ navbarLocation.innerHTML = `<div class="container-fluid">
 <button class="navbar-toggler toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
   <span class="navbar-toggler-icon"></span>
 </button>
-<a class="navbar-brand mx-auto" href="#">
+<a class="navbar-brand mx-auto" href="index.html">
   <img src="./assets/img/LogoNavbar.svg" alt="">
 </a>
 <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
@@ -48,7 +48,7 @@ footerLocation.innerHTML=`<div class="col-12 d-none d-lg-block">
 </div>
 </div>
 <div class="col-md-12 col-lg-2">
-<a href="" class="d-flex justify-content-center">
+<a href="index.html" class="d-flex justify-content-center">
   <img id="footerLogo" src="./assets/img/LogoBatuq.svg" alt="">
 </a>
 </div>
@@ -71,6 +71,10 @@ footerLocation.innerHTML=`<div class="col-12 d-none d-lg-block">
 </div>
 </div>
 <div class="col-md-12 col-lg-2 d-flex justify-content-center">
-<a href="https://www.facebook.com/BacanoraBatuq" target="_blank">Facebook logo</a>
-<a href="https://www.instagram.com/bacanorabatuq/" target="_blank">Instagram Logo</a>
-</div>`;
+<a href="https://www.facebook.com/BacanoraBatuq" target="_blank"><img src="./assets/img/Facebook.svg" alt=""></a>
+<a href="https://www.instagram.com/bacanorabatuq/" target="_blank"><img src="./assets/img/IG.svg" alt=""></a>
+</div>
+<div id="image-atr">
+<a href="https://www.freepik.com/free-vector/social-media-icons-vector-set-with-facebook-instagram-twitter-tiktok-youtube-logos_17221200.htm#" target="_blank">Image by rawpixel.com</a> on Freepik
+</div>
+`;
